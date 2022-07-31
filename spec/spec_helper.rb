8d7596a57e447f42a90a55cbc22204235dd486d2
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-07-31 17:09:19 +0200

    Better rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,27 +10,7 @@ require "tncl/func"
 
 Zeitwerk::Loader.eager_load_all
 
-# pp Dir["#{__dir__}}/support/**/*.rb"]
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| load(f) }
-
-shared_files = Dir["#{File.dirname(__FILE__)}/fixtures/functions/ruby/base/*"]
-
-Dir["#{File.dirname(__FILE__)}/fixtures/functions/ruby/*"].each do |fn|
-  name = File.basename(fn)
-  next if name == "base" # skip shared for all functions stuff
-
-  shared_files.each do |f|
-    FileUtils.copy(f, fn)
-  end
-
-  Dir.chdir(fn) do
-    puts("Building test function #{name}...")
-    `docker build . -t #{name}`
-    next if $CHILD_STATUS.success?
-
-    raise "Cannot build function '#{name}'"
-  end
-end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -44,4 +24,25 @@ RSpec.configure do |config|
   end
 
   config.include_context(Async::RSpec::Reactor)
+
+  config.before(:suite) do
+    shared_files = Dir["#{File.dirname(__FILE__)}/fixtures/functions/ruby/base/*"]
+
+    Dir["#{File.dirname(__FILE__)}/fixtures/functions/ruby/*"].each do |fn|
+      name = File.basename(fn)
+      next if name == "base" # skip shared for all functions stuff
+
+      shared_files.each do |f|
+        FileUtils.copy(f, fn)
+      end
+
+      Dir.chdir(fn) do
+        puts("Building test function #{name}...")
+        `docker build . -t #{name}`
+        next if $CHILD_STATUS.success?
+
+        raise "Cannot build function '#{name}'"
+      end
+    end
+  end
 end
